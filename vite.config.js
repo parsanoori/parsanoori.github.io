f34bfa7b889eb4ff3ffd8e5e2222294b553cce0a
Parsa Noori <parsanoori79@gmail.com>
2025-06-07 01:44:02 +0330

    added the deploy command

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/', // or '/portfolio/' if using a repo page
+  plugins: [react()],
 })
+
